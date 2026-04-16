3147cabd6a719fa630c8574496fcd62e945cf110
Madhavan-60 <madhavankannan73@gmail.com>
2026-04-16 10:34:11 +0530

    Added Zigzag_Conversion solution 1

---
+++ Zigzag_Conversion/solution1.py
@@ -0,0 +1,33 @@
+        }
+
+        int idx = 0, d = 1;
+        List<Character>[] rows = new ArrayList[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new ArrayList<>();
+        }
+
+        for (char c : s.toCharArray()) {
+            rows[idx].add(c);
+            return s;
+       if (numRows == 1 || numRows >= s.length()) {
+            if (idx == 0) {
+                d = 1;
+            } else if (idx == numRows - 1) {
+                d = -1;
+            }
+            idx += d;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (List<Character> row : rows) {
+            for (char c : row) {
+                result.append(c);
+            }
+        }
+
+        return result.toString();        
+    }
+}
+    public String convert(String s, int numRows) {
+class Solution {
+
