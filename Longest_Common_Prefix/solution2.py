ed2993951be418a8098224b30a8205a70a3ee799
Madhavan-60 <madhavankannan73@gmail.com>
2026-04-16 10:20:19 +0530

    Added Longest_Common_Prefix solution 2

---
+++ Longest_Common_Prefix/solution2.py
@@ -0,0 +1,13 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        
+        String pref = strs[0];
+        int prefLen = pref.length();
+
+        for (int i = 1; i < strs.length; i++) {
+            String s = strs[i];
+            while (prefLen > s.length() || !pref.equals
+(s.substring(0, prefLen))) {
+                prefLen--;
+
